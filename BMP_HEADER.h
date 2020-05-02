370fa74df3cecb426d5f85b2c2f9b9a894723d33
Niebowziemii <59135705+Niebowziemii@users.noreply.github.com>
2020-05-02 20:47:55 +0200

    Update BMP_HEADER.h
    update

--- BMP_HEADER.h
+++ BMP_HEADER.h
@@ -4,171 +4,223 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
-#ifndef BFTYPE
-#define BFTYPE *((uint16_t*)&buffer[0])
+
+#ifdef BFTYPE
+#undef BFTYPE
 #endif // !BFTYPE
-#ifndef BFSIZE
-#define BFSIZE *((uint32_t*)&buffer[2])
+#define BFTYPE(name) *((uint16_t*)&name[0])
+
+#ifdef BFSIZE
+#undef BFSIZE
 #endif // !BFSIZE
-#ifndef BFRESERVED1
-#define BFRESERVED1 *((uint16_t*)&buffer[6])
+#define BFSIZE(name) *((uint32_t*)&name[2])
+
+#ifdef BFRESERVED1
+#undef BFRESERVED1
 #endif // !BFRESERVED1
-#ifndef BFRESERVED2
-#define BFRESERVED2 *((uint16_t*)&buffer[8])
+#define BFRESERVED1(name) *((uint16_t*)&name[6])
+
+#ifdef BFRESERVED2
+#undef BFRESERVED2
 #endif // !BFRESERVED2
-#ifndef BFOFFBITS
-#define BFOFFBITS *((uint32_t*)&buffer[10])
+#define BFRESERVED2(name) *((uint16_t*)&name[8])
+
+#ifdef BFOFFBITS
+#undef BFOFFBITS
 #endif // !BFOFFBITS
-#ifndef BISIZE
-#define BISIZE *((uint32_t*)&buffer[14])
+#define BFOFFBITS(name) *((uint32_t*)&name[10])
+
+#ifdef BISIZE
+#undef BISIZE
 #endif // !BISIZE
-#ifndef BIWIDTH
-#define BIWIDTH *((uint32_t*)&buffer[18])
+#define BISIZE(name) *((uint32_t*)&name[14])
+
+#ifdef BIWIDTH
+#undef BIWIDTH
 #endif // !BIWIDTH
-#ifndef BIHEIGHT
-#define BIHEIGHT *((uint32_t*)&buffer[22])
+#define BIWIDTH(name) *((uint32_t*)&name[18])
+
+#ifdef BIHEIGHT
+#undef BIHEIGHT
 #endif // !BIHEIGHT
-#ifndef BIPLANES
-#define BIPLANES *((uint16_t*)&buffer[26])
+#define BIHEIGHT(name) *((uint32_t*)&name[22])
+
+#ifdef BIPLANES
+#undef BIPLANES
 #endif// !BIPLANES
-#ifndef BIBITCOUNT
-#define BIBITCOUNT *((uint16_t*)&buffer[28])
+#define BIPLANES(name) *((uint16_t*)&name[26])
+
+#ifdef BIBITCOUNT
+#undef BIBITCOUNT
 #endif// !BIBITCOUNT
-#ifndef BICOMPRESSION
-#define BICOMPRESSION *((uint32_t*)&buffer[30])
+#define BIBITCOUNT(name) *((uint16_t*)&name[28])
+
+#ifdef BICOMPRESSION
+#undef BICOMPRESSION
 #endif// !BICOMPRESSION
-#ifndef BISIZEIMAGE
-#define BISIZEIMAGE *((uint32_t*)&buffer[34])
+#define BICOMPRESSION(name) *((uint32_t*)&name[30])
+
+#ifdef BISIZEIMAGE
+#undef BISIZEIMAGE
 #endif// !BISIZEIMAGE
-#ifndef BIXPERLSPERMETER
-#define BIXPERLSPERMETER *((uint32_t*)&buffer[38])
+#define BISIZEIMAGE(name) *((uint32_t*)&name[34])
+
+#ifdef BIXPERLSPERMETER
+#undef BIXPERLSPERMETER
 #endif// !BIXPERLSPERMETER
-#ifndef BIYPERLSPERMETER
-#define BIYPERLSPERMETER *((uint32_t*)&buffer[42])
+#define BIXPERLSPERMETER(name) *((uint32_t*)&name[38])
+
+#ifdef BIYPERLSPERMETER
+#undef BIYPERLSPERMETER
 #endif// !BIYPERLSPERMETER
-#ifndef BICLRUSED
-#define BICLRUSED *((uint32_t*)&buffer[46])
+#define BIYPERLSPERMETER(name) *((uint32_t*)&name[42])
+
+#ifdef BICLRUSED
+#undef BICLRUSED
 #endif// !BICLRUSED
-#ifndef BICLRIMPORTANT
-#define BICLRIMPORTANT *((uint32_t*)&buffer[50])
+#define BICLRUSED(name) *((uint32_t*)&name[46])
+
+#ifdef BICLRIMPORTANT
+#undef BICLRIMPORTANT
 #endif// !BICLRIMPORTANT
-#ifndef IF
+#define BICLRIMPORTANT(name) *((uint32_t*)&name[50])
+
+#ifndef IF(num)
 #define IF(num) if(a >= num*16 && a<=(num+1)*16 - 1) { return num; }
 #endif
-void printData(char* buffer) {
-    printf("BITMAPFILEHEADER: \nbfType: \t\t0x%X\t%d (BM)\nbfSize: \t\t0x%X\t%d\nbfReserved1: \t\t0x%X\t%d\nbfReserved2: \t\t0x%X\t%d\nbfOffBits: \t\t0x%X\t%d\n\n", BFTYPE, BFTYPE, BFSIZE, BFSIZE, BFRESERVED1, BFRESERVED1, BFRESERVED2, BFRESERVED2, BFOFFBITS, BFOFFBITS);
-    printf("BITMAPINFOHEADER: \nbiSize: \t\t0x%X\t%d\nbiWidth: \t\t0x%X\t%d\nbiHeight: \t\t0x%X\t%d\nbiPlanes: \t\t0x%X\t%d\nbiBitCount: \t\t0x%X\t%d\nbiCompression: \t\t0x%X\t%d\nbiSizeImage: \t\t0x%X\t%d\nbiXPelsPerMeter: \t0x%X\t%d\nbiYPelsPerMeter: \t0x%X\t%d\nbiClrUsed: \t\t0x%X\t%d\nbiClrImportant: \t0x%X\t%d\n", BISIZE, BISIZE, BIWIDTH, BIWIDTH, BIHEIGHT, BIHEIGHT, BIPLANES, BIPLANES, BIBITCOUNT, BIBITCOUNT, BICOMPRESSION, BICOMPRESSION, BISIZEIMAGE, BISIZEIMAGE, BIXPERLSPERMETER, BIXPERLSPERMETER, BIYPERLSPERMETER, BIYPERLSPERMETER, BICLRUSED, BICLRUSED, BICLRIMPORTANT, BICLRIMPORTANT);
+void print_data(char* buffer) {
+    printf("BITMAPFILEHEADER:\
+            \
+            \nbf_type: \t\t0x%X\t%d (BM)\n\
+            bf_size: \t\t0x%X\t%d\n\
+            bf_reserved1: \t\t0x%X\t%d\n\
+            bf_reserved2: \t\t0x%X\t%d\n\
+            bf_off_bits: \t\t0x%X\t%d\n\n",\
+            BFTYPE(buffer), BFTYPE(buffer),\
+            BFSIZE(buffer), BFSIZE(buffer),\
+            BFRESERVED1(buffer), BFRESERVED1(buffer),\
+            BFRESERVED2(buffer), BFRESERVED2(buffer),\
+            BFOFFBITS(buffer), BFOFFBITS(buffer));
+
+    printf("BITMAPINFOHEADER:\
+            \
+            \nbi_size: \t\t0x%X\t%d\n\
+            bi_width: \t\t0x%X\t%d\n\
+            bi_height: \t\t0x%X\t%d\n\
+            bi_planes: \t\t0x%X\t%d\n\
+            bi_bit_count: \t\t0x%X\t%d\n\
+            bi_compression: \t\t0x%X\t%d\n\
+            bi_size_image: \t\t0x%X\t%d\n\
+            bi_x_pels_per_meter: \t0x%X\t%d\n\
+            bi_y_pels_per_meter: \t0x%X\t%d\n\
+            bi_clr_used: \t\t0x%X\t%d\n\
+            bi_clr_important: \t0x%X\t%d\n",\
+            BISIZE(buffer), BISIZE(buffer),\
+            BIWIDTH(buffer), BIWIDTH(buffer),\
+            BIHEIGHT(buffer), BIHEIGHT(buffer),\
+            BIPLANES(buffer), BIPLANES(buffer),\
+            BIBITCOUNT(buffer), BIBITCOUNT(buffer),\
+            BICOMPRESSION(buffer), BICOMPRESSION(buffer),\
+            BISIZEIMAGE(buffer), BISIZEIMAGE(buffer),\
+            BIXPERLSPERMETER(buffer), BIXPERLSPERMETER(buffer),\
+            BIYPERLSPERMETER(buffer), BIYPERLSPERMETER(buffer),\
+            BICLRUSED(buffer), BICLRUSED(buffer),\
+            BICLRIMPORTANT(buffer), BICLRIMPORTANT(buffer));
 }
-#ifdef linux
-void strreverse(char* begin, char* end) {
-    char aux;
-    while (end > begin)
-        aux = *end, * end-- = *begin, * begin++ = aux;
-}
-void itoa(int value, char* str, int base) {
-    static char num[] = "0123456789abcdefghijklmnopqrstuvwxyz";
-    char* wstr = str;
-    int sign;
-    // Validate base
-    if (base < 2 || base>35) { *wstr = '\0'; return; }
-    // Take care of sign
-    if ((sign = value) < 0) value = -value;
-    // Conversion. Number is reversed.
-    do *wstr++ = num[value % base]; while (value /= base);
-    if (sign < 0) *wstr++ = '-';
-    *wstr = '\0';
-    // Reverse string
-    strreverse(str, wstr - 1);
-}
-#endif
-int checkNum(unsigned char* num) {
+int check_num(unsigned char* num) {
     int a = (int)*num;
-    for (int i = 0; i < 16; i++) {
-        IF(i);
-    }
-    return EXIT_FAILURE;
+    _asm mov eax, a
+    _asm push ebx
+    _asm push ecx
+    _asm xor ebx, ebx
+    _asm loop_start:
+    _asm mov ecx, ebx
+    _asm imul ecx, 0x00000010
+    _asm cmp eax, ecx
+    _asm jl loop_check
+    _asm mov ecx, ebx
+    _asm inc ecx
+    _asm imul ecx, 0x00000010
+    _asm dec ecx
+    _asm cmp eax, ecx
+    _asm jg loop_check
+    _asm mov eax, ebx
+    _asm pop ecx
+    _asm pop ebx
+    _asm jmp _return_
+    _asm loop_check:
+    _asm inc ebx
+    _asm cmp ebx, 0x00000010
+    _asm jb loop_start
+    _asm pop ecx
+    _asm pop ebx
+    _asm xor eax, eax
+    _asm inc eax
+    _asm _return_:
+    _asm mov a, eax
+    return a;
 }
-void printIt(int* counter, int sum, const char* mess) {
+void print_histogram_colour_data(int* counter, int sum, const char* mess) {
     printf("\n\n%s: \n\n", mess);
     for (int i = 0, j = 0; i < 16; j++, i++)
         printf("%d-%d: %.2lf%%\n", 16 * i, 16 * i + 15, (counter[j] / (double)sum) * 100);
 }
-void histogramate(char* buffer) {
-    int offset = BFSIZE - BISIZEIMAGE;
-    int padding = ((BIWIDTH * 3) % 4);
-    int countBlue[16] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-    int countGreen[16] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-    int countRed[16] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+void generate_histogram(char* buffer) {
+    int offset = BFSIZE(buffer) - BISIZEIMAGE(buffer);
+    int padding = ((BIWIDTH(buffer) * 3) % 4);
+    int count_blue[16] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    int count_green[16] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    int count_red[16] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     // for every row
-    for (int i = 0; i < BIHEIGHT; i++) {
+    for (unsigned i = 0; i < BIHEIGHT(buffer); i++) {
         // for every pixel (triple)
-        for (int j = 0; j < BIWIDTH * 3; j += 3) {
-            unsigned char buffBlue;
+        for (unsigned j = 0; j < BIWIDTH(buffer) * 3; j += 3) {
+            unsigned char buff_blue = (unsigned char)(buffer[offset + j + i * (BIWIDTH(buffer) * 3 + padding)]);
             int b = 0;
-            memset(&buffBlue, 0, sizeof(char));
-            memcpy(&buffBlue, (const char*)(buffer + offset + j + i * (BIWIDTH * 3 + padding)), 1);
-            b = checkNum(&buffBlue);
-            countBlue[b]++;
+            b = check_num(&buff_blue);
+            count_blue[b]++;
 
-            unsigned char buffGreen;
-            memset(&buffGreen, 0, sizeof(char));
-            memcpy(&buffGreen, (const char*)(buffer + offset + j + i * (BIHEIGHT * 3 + padding) + 1), 1);
-            b = checkNum(&buffGreen);
-            countGreen[b]++;
+            unsigned char buff_green = (unsigned char)(buffer[offset + j + i * (BIWIDTH(buffer) * 3 + padding)+1]);
+            b = check_num(&buff_green);
+            count_green[b]++;
 
-            unsigned char buffRed;
-            memset(&buffRed, 0, sizeof(char));
-            memcpy(&buffRed, (const char*)(buffer + offset + j + i * (BIWIDTH * 3 + padding) + 2), 1);
-            b = checkNum(&buffRed);
-            countRed[b]++;
-            //printf("Blue: %X Green: %X Red: %X\n", buffBlue, buffGreen, buffRed);
+            unsigned char buff_red = (unsigned char)(buffer[offset + j + i * (BIWIDTH(buffer) * 3 + padding)+2]);
+            b = check_num(&buff_red);
+            count_red[b]++;
+            //printf("Blue: %X Green: %X Red: %X\n", buff_blue, buff_green, buff_red);
         }
-        //printf("End of row. %d\n",i);
     }
     // obtain sum of all pixel values to check correctness 
     int sum = 0;
     for (int i = 0; i < 16; i++)
-        sum += countBlue[i];
+        sum += count_blue[i];
     // handle error
-    if (BIHEIGHT * BIWIDTH != sum) {
-        printf("Number of pixels in the histogram does noot match number of pixels in the image");
+    if (BIHEIGHT(buffer) * BIWIDTH(buffer) != sum) {
+        printf("Number of pixels in the histogram does not match number of pixels in the image");
     }
-    printIt(countBlue, sum, "Blue");
-    printIt(countGreen, sum, "Green");
-    printIt(countRed, sum, "Red");
+    print_histogram_colour_data(count_blue, sum, "Blue");
+    print_histogram_colour_data(count_green, sum, "Green");
+    print_histogram_colour_data(count_red, sum, "Red");
 }
-void grayScaleMe(char* buffer, FILE * rfile) {
+void turn_file_into_grayscale(char* buffer, FILE * rfile) {
     // copy headers
-    fwrite(buffer, 14 + BISIZE, 1, rfile);
-    int offset = BFSIZE - BISIZEIMAGE;
-    int padding = ((BIWIDTH * 3) % 4);
-    // go through pixels
-    for (int i = 0; i < BIHEIGHT; i++) {
-        for (int j = 0; j < BIWIDTH * 3; j += 3) {
-            unsigned char buffBlue;
-            memset(&buffBlue, 0, sizeof(char));
-            memcpy(&buffBlue, (const char*)(buffer + offset + j + i * (BIWIDTH * 3 + padding)), 1);
-            unsigned char buffGreen;
-            memset(&buffGreen, 0, sizeof(char));
-            memcpy(&buffGreen, (const char*)(buffer + offset + j + i * (BIWIDTH * 3 + padding) + 1), 1);
-            unsigned char buffRed;
-            memset(&buffRed, 0, sizeof(char));
-            memcpy(&buffRed, (const char*)(buffer + offset + j + i * (BIWIDTH * 3 + padding) + 2), 1);
-            int mean = ((int)buffBlue + (int)buffGreen + (int)buffRed) / 3;
-            char g[3];
-#ifdef linux
-            itoa(mean, g, 16);
-#endif 
-#ifdef _WIN32
-            _itoa(mean, g, 16);
-#endif
-            fwrite(g, 1, 1, rfile);
-            fwrite(g, 1, 1, rfile);
-            fwrite(g, 1, 1, rfile);
+    fwrite(buffer, 14 + BISIZE(buffer), 1, rfile);
+    int offset = BFSIZE(buffer) - BISIZEIMAGE(buffer);
+    int padding = ((BIWIDTH(buffer) * 3) % 4);
+    // insert pixels
+    for (int i = 0; i < BIHEIGHT(buffer); i++) {
+        for (int j = 0; j < BIWIDTH(buffer) * 3; j += 3) {
+            unsigned char buff_blue = (unsigned char)(buffer[offset + j + i * (BIWIDTH(buffer) * 3 + padding)]);
+            unsigned char buff_green = (unsigned char)(buffer[offset + j + i * (BIWIDTH(buffer) * 3 + padding) + 1]);
+            unsigned char buff_red = (unsigned char)(buffer[offset + j + i * (BIWIDTH(buffer) * 3 + padding) + 2]);
+            const char mean = (buff_blue + buff_green + buff_red) / 3;
+            fwrite(&mean, 1, 1, rfile);
+            fwrite(&mean, 1, 1, rfile);
+            fwrite(&mean, 1, 1, rfile);
         }
     }
 }
-FILE* fileCreate(char** argv, int index, const char* mode) {
+FILE* create_a_file_from_name_stored_in_main_argument_list(char** argv, int index, const char* mode) {
     FILE* file = NULL;
     file = fopen(argv[index], mode);
     if (!file) {
@@ -177,32 +229,9 @@ FILE* fileCreate(char** argv, int index, const char* mode) {
     }
     return file;
 }
-char* allocateBuffer(uint32_t size) {
-    char* buffer;
-    buffer = (char*)malloc(size * sizeof(char));
-    memset(buffer, 0, size * sizeof(char));
-    if (buffer == NULL) {
-        printf("Memory error.");
-        return NULL;
-    }
-    return buffer;
-}
-int fileDestroy(FILE * file) {
-    if (file) {
-        int err;
-        err = fclose(file);
-        if (!err) {
-            printf("The file was closed.");
-            return EXIT_SUCCESS;
-        }
-        else {
-            printf("Error in closing the file.");
-            return EXIT_FAILURE;
-        }
-    }
-}
-int validateFile(char* buffer) {
-    if (BFTYPE != 19778) {
+
+int validate_file(char* buffer) {
+    if (BFTYPE(buffer) != 19778) {
         printf("This is not a BMP file");
         return EXIT_FAILURE;
     }
