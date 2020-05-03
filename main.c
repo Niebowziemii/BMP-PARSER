fd6e3ab78291d8daee74f4734821c9a2b4ce6f00
Niebowziemii <59135705+Niebowziemii@users.noreply.github.com>
2020-05-03 15:04:43 +0200

    Update main.c
    it should now work on linux

--- main.c
+++ main.c
@@ -3,39 +3,40 @@ int main(int argc, char** argv) {
 
     // create an empty file
     FILE* file = create_a_file_from_name_stored_in_main_argument_list(argv, 1, "rb");
-    
+    if (file == NULL) {
+        return EXIT_FAILURE;
+    }
+    struct stat buf;
     // obtain file size
-    char buff[6];
-    fgets(buff, 6, file);
-    uint32_t* fsize = (uint32_t*)(buff+2);
-    rewind(file);
-
-    // validate file
-    if (validate_file(buff)) { return EXIT_FAILURE; }
+    if ((fstat(fileno(file), &buf) != 0) ||(!S_ISREG(buf.st_mode))) {
+        fprintf(stderr, "[error]This will work only with regular file.");
+            return EXIT_FAILURE;
+    }
+    off_t* fsize = &buf.st_size;
 
     // allocate memory for the file
     char* buffer;
     buffer = (char*)malloc(*fsize * sizeof(char));
     if (buffer == NULL) {
-        printf("Cannot allocate memory for file content.");
+        printf("[error]Cannot allocate memory for file content.");
         return EXIT_FAILURE;
     }
 
     // copy the content to the buffer
-    size_t ostensible_file_size= fread(buffer, 1,(*fsize), file);
+    size_t ostensible_file_size = fread(buffer, 1, (*fsize), file);
     if (ostensible_file_size != *fsize) {
-        printf("Unable to read a file.");
+        printf("[error]Unable to read a file.");
         return EXIT_FAILURE;
     }
-
+    if (validate_file(buffer)) { return EXIT_FAILURE; }
     // print data and histogram from requirements if 1 command line argument
     if (argc == 2) {
         print_data(buffer);
-        if (BICOMPRESSION(buffer) == 0 && BIBITCOUNT(buffer) == 24){
+        if (BICOMPRESSION(buffer) == 0 && BIBITCOUNT(buffer) == 24) {
             generate_histogram(buffer);
         }
-        else{
-            printf("Histogram calculation is unsupported.");
+        else {
+            printf("[error]Histogram calculation is unsupported.");
         }
     }
 
@@ -46,11 +47,11 @@ int main(int argc, char** argv) {
             if (BICOMPRESSION(buffer) == 0 && BIBITCOUNT(buffer) == 24)
                 create_a_copy_of_given_file_in_grayscale(buffer, result_file);
             else
-                printf("Grayscale convertion is unsupported.");
+                printf("[error]Grayscale convertion is unsupported.");
             fclose(result_file);
         }
         else {
-            printf("Result file cannot be created.");
+            printf("[error]Result file cannot be created.");
             return EXIT_FAILURE;
         }
     }
